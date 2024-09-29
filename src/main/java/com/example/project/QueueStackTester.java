cbb70d7f8714449f9f7991dec4cdf9410dfaad9e
Yousef-AlHamed <alhamedyou@gmail.com>
2024-09-29 16:11:18 +0300

    first commit

--- src/main/java/com/example/project/QueueStackTester.java
+++ src/main/java/com/example/project/QueueStackTester.java
@@ -3,29 +3,35 @@ package com.example.project;
 public class QueueStackTester {
     public static <T> void split(Queue<T> q, Queue<T> oq, Queue<T> eq)
     {
-        throw new UnsupportedOperationException("Not supported yet.");
-        // Write the recursive static method split that splits a queue of n elements into two
-        // queues. The elements with odd orders (i.e. 1st, 3rd, 5th ...) should be put in the
-        // first queue and elements with even orders (i.e. 2nd, 4th, 6th ...) should be put in
-        // the second queue. The original queue should remain unchanged at the end of the
-        // method.
-        // Example. Given the queue (A, B, C, D, E), split results in oq (A, C, E), and eq(B, D).
+       for (int counter = 1; counter <= q.length(); counter++) {
+    	   T x = q.serve();
+    	   q.enqueue(x);
+    	   
+    	   if (counter % 2 == 1) {
+    		   oq.enqueue(x);
+    	   } else {
+    		   eq.enqueue(x);
+    	   }
+       };
+       
     }
     public static <T> void remove(LinkedPQ<T> pq, int p)
     {
-        throw new UnsupportedOperationException("Not supported yet.");
-        // Write a static method remove that removes every element in the priority queue
-        // having priority less than p.
-        // Example. Given pq: [A, 10], [D, 8], [B, 5], [E, 3], [C, 2] remove(pq, 5) results in
-        // pq: [A, 10], [D, 8], [B, 5].
+    	for (int i = 0; i < pq.length(); i++) {
+    		PQElement<T> x = pq.serve();
+    		if (x.p >= p)
+    			pq.enqueue(x.data, x.p);
+    	}
     }
     public static <T> boolean search(Stack<T> st, T e)
     {
-        throw new UnsupportedOperationException("Not supported yet.");
-        // Write the recursive static method search that searches for an element e in a stack st
-        // and returns true if it’s found or false otherwise. st should not change at the end of
-        // the method. You are not allowed to use any auxiliary data structures.
-        // Example. Given the stack st (top-to-bottom): 5, 7, 5, 3, 2. search(st, 3) returns
-        // true while search(st, 1) returns false.
+			if (st.empty())
+				return false;
+			T x = st.pop();
+			boolean flag = QueueStackTester.search(st, e);
+			if (x.equals(e))
+				flag = true;
+			st.push(x);
+			return flag;
     }
 }
